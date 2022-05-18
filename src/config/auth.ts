4564f258d85e89e9cb9eb999b47daf2da0064634
Thifany-Nicastro <thifanynicastro@hotmail.com>
2022-05-18 10:42:36 -0300

    Refatorando Autenticação do usuário

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "d652eeeea9a382e2b37ad73e0a66b131",
+  expires_in_token: "15m",
+  secret_refresh_token: "125c41faf66e2c351bff02def3b22cab",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
